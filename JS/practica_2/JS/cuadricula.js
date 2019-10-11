6c07256726a9fe5308158cb6d44ad88a52e3a353
Gilberberto <gil_millansolis2@hotmail.com>
2019-10-11 09:40:40 -0500

    version0.5 cuadricula

--- JS/practica_2/JS/cuadricula.js
+++ JS/practica_2/JS/cuadricula.js
@@ -1,6 +1,9 @@
 		$(document).ready(function (){
 
+			$(".cuadro").css({"height":"90px","width":"90px","border":"10px solid cyan","display":"inline-block"});
+			$("#btn1").function({
+				$()
+			})
 
-			$(".cuadro").css({"height":"90px","width":"90px","border":"10px cyan","display":"inline-block"});
+		})
 
-		}
